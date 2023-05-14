80e149a264fd33a39acab7e53c8d0a8055197cff
Misha2048 <mikalan.itsmart@gmail.com>
2023-05-14 15:53:33 +0300

    Link amount fixed

--- routes/links.py
+++ routes/links.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from typing import List
 import threading
-import asyncio
 from repo.links import LinkRepo
 from models.links import  HTMLModel
 from helpers.get_html_by_links import get_html_by_link
@@ -10,32 +9,32 @@ from helpers.async_wrapper import async_wrapper
 
 link_router = APIRouter()
 
-async def insert_noncached(link: str, target: List) -> None:
-    html = get_html_by_link(link)
-    if html:
-        document = await LinkRepo.insert_html(link, html)
-        target.append(document)
-    else:
-        target.append({"link": link, "html": "No content. Please, check if url is correct"})
+
+async def parse_urls(html: List, link: str):
+    link = convert_link(link)
+    try:
+        if document := await LinkRepo.get_html(link):
+            html.append(document)
+        else:
+            html_text = get_html_by_link(link)
+            if html_text:
+                document = await LinkRepo.insert_html(link, html_text)
+                html.append(document)
+            else:
+                html.append({"link": link, "html": "No content. Please, check if url is correct"})
+    except:
+        html.append({"link": link, "html": "Error was occurred"})
 
 
 @link_router.post("/parse")
 async def get_html(links: List[str]) -> List[HTMLModel]:
     if len(links) > 50:
-        links = links[:51]
+        links = links[:50]
 
     html = []
 
     for link in links:
-        link = convert_link(link)
-        try:
-            if document := await LinkRepo.get_html(link):
-                html.append(document)
-            else:
-                thr = threading.Thread(target=async_wrapper, args=(insert_noncached, link, html))
-                thr.start()
-                thr.join()
-                # await insert_noncached(link, html)
-        except:
-            html.append({"link": link, "html": "Error was occurred"})
+        thr = threading.Thread(target=async_wrapper, args=(parse_urls, html, link))
+        thr.start()
+        thr.join()
     return html
